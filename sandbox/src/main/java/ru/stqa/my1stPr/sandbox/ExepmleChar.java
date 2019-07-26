aa7c21301a4e12a9fa482f89440fbcbf60d1826f
Daskendrik <Daskendrik@users.noreply.github.com>
2019-07-26 16:17:37 +0300

    Пример с симв

---
+++ sandbox/src/main/java/ru/stqa/my1stPr/sandbox/ExepmleChar.java
@@ -0,0 +1,12 @@
+package ru.stqa.my1stPr.sandbox;
+// Пример символов, их можно обозначать в '', а можно просто написать код символа
+public class ExepmleChar {
+    public static void main(String[] args) {
+
+        char ch1, ch2;
+        ch1 = 88; // 88 это вроде бы код символа Х
+        ch2 = 'Y';
+        System.out.print("ch1 and ch2: ");
+        System.out.println(ch1 + " " + ch2);
+    }
+}
